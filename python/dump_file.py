83831ec8d01ee884582690e274ffed20d60197f9
milahu <milahu@gmail.com>
2022-06-13 14:33:28 +0200

    Create dump_file.py

---
+++ python/dump_file.py
@@ -0,0 +1,7 @@
+# https://stackoverflow.com/questions/43197657/printing-out-each-line-from-file-with-line-number-python
+
+def dump_file(file):
+    "print file contents with line numbers"
+    with open(file, "r") as fh:
+        for num, line in enumerate(fh, start=1):
+            print(f"{file}:{num} {line}", end="")
