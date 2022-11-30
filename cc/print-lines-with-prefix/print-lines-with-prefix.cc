8cf4ddb76e045c0ed68a522d66b2ad282049f92f
Milan Hauth <milahu@gmail.com>
2022-11-30 09:23:48 +0100

    up

--- cc/print-lines-with-prefix/print-lines-with-prefix.cc
+++ cc/print-lines-with-prefix/print-lines-with-prefix.cc
@@ -3,7 +3,7 @@ print lines with prefix
 
 example output
 
-g++ -g main.cc -o main && ./main
+g++ -g print-lines-with-prefix.cc -o print-lines-with-prefix && ./print-lines-with-prefix
 
 process 2981423: a
 process 2981423: b
@@ -21,6 +21,8 @@ process 2981423: b
 
 #include <sys/types.h> // getpid
 #include <unistd.h> // getpid
+
+//#include <stdio.h> // stdout
 
 // https://stackoverflow.com/a/27336473/10440128
 class prefixbuf
@@ -67,23 +69,41 @@ public:
 class MainClass
 {
   private:
-    const char* s;
+    const char *s;
+    char line_prefix_[32];
+    char *line_prefix_ptr_;
+    //std::string line_prefix_string_;
     oprefixstream *oprefixstream_;
   public:
     MainClass()
       : s("a\nb\n")
     {
-      char line_prefix[100];
       pid_t pid = getpid();
-      assert(snprintf(line_prefix, 100, "process %d: ", pid) < 100);
 
-      oprefixstream_ = new oprefixstream(line_prefix, std::cout);
+      assert(snprintf(line_prefix_, 32, "pid %d: ", pid) < 32);
+      printf("line_prefix_ = '%s'\n", line_prefix_);
+
+      line_prefix_ptr_ = (char *) malloc(32);
+      snprintf(line_prefix_ptr_, 32, "pid %d: ", pid);
+      printf("line_prefix_ptr_ = '%s'\n", line_prefix_ptr_);
+
+      oprefixstream_ = new oprefixstream(line_prefix_, std::cout);
+      //oprefixstream_ = new oprefixstream(line_prefix, std::cout);
+      //oprefixstream_ = new oprefixstream(line_prefix, std::cerr);
     }
     void Print() {
       *oprefixstream_ << s;
       *oprefixstream_ << "1\n2\n";
       (*oprefixstream_).write(&s[0], 4);
       oprefixstream_->write(&s[0], 4);
+      char a[10];
+      a[0] = 'a';
+      a[1] = 's';
+      a[2] = 'd';
+      a[3] = 'f';
+      a[4] = 0;
+      oprefixstream_->write(a, 4);
+      oprefixstream_->flush();
     }
 };
 
