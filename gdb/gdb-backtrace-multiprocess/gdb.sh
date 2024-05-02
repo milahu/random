e137e29a9738bc539b99aaa0afc48397a7e07ec1
Milan Hauth <milahu@gmail.com>
2024-05-02 14:43:28 +0200

    add catchpoints to every inferior

--- gdb/gdb-backtrace-multiprocess/gdb.sh
+++ gdb/gdb-backtrace-multiprocess/gdb.sh
@@ -3,34 +3,36 @@
 args=(
 gdb
 --quiet
+-ex "set breakpoint pending on"
 -ex "break execvp"
+-ex "run"
+
+# break at execvp
 -ex "catch syscall exit"
 -ex "set follow-exec-mode new"
 #-ex "set follow-fork-mode child"
--ex "set breakpoint pending on"
 -ex "set disassemble-next-line on"
 -ex "break abort"
 -ex "break fatal_error"
--ex "catch syscall exit"
--ex "catch syscall _exit"
 -ex "catch syscall exit_group"
--ex "catch syscall execvp"
 #-ex "catch syscall group:process"
 #-ex "set detach-on-fork off"
--ex "run"
-# break at execvp
 #-ex 'where'
 #-ex 'info inferiors'
 -ex 'info threads'
 -ex 'printf "rdi = %s\n", $rdi' # first argument of execvp
 -ex 'c'
+
 # break at execvp
 #-ex 'where'
 #-ex 'info inferiors'
 -ex 'info threads'
 -ex 'printf "rdi = %s\n", $rdi' # first argument of execvp
+-ex "catch syscall exit_group"
 -ex 'c'
-# FIXME no more break before exit
+
+# break at exit
+-ex 'bt'
 --args ./prog1
 )
 
