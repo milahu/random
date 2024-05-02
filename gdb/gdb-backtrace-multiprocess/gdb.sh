bc4c254728155203d19cfc9ea167267a0bfadfa6
Milan Hauth <milahu@gmail.com>
2024-05-02 14:48:17 +0200

    use breakpoints instead of catchpoints

--- gdb/gdb-backtrace-multiprocess/gdb.sh
+++ gdb/gdb-backtrace-multiprocess/gdb.sh
@@ -5,16 +5,17 @@ gdb
 --quiet
 -ex "set breakpoint pending on"
 -ex "break execvp"
+-ex "break exit"
 -ex "run"
 
 # break at execvp
--ex "catch syscall exit"
+#-ex "catch syscall exit"
 -ex "set follow-exec-mode new"
 #-ex "set follow-fork-mode child"
 -ex "set disassemble-next-line on"
 -ex "break abort"
 -ex "break fatal_error"
--ex "catch syscall exit_group"
+#-ex "catch syscall exit_group"
 #-ex "catch syscall group:process"
 #-ex "set detach-on-fork off"
 #-ex 'where'
@@ -28,7 +29,7 @@ gdb
 #-ex 'info inferiors'
 -ex 'info threads'
 -ex 'printf "rdi = %s\n", $rdi' # first argument of execvp
--ex "catch syscall exit_group"
+#-ex "catch syscall exit_group"
 -ex 'c'
 
 # break at exit
