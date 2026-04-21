715d62091ebed5782b3fa27b7ae58822f3fc3a56
Milan Hauth <milahu@milahu.duckdns.org>
2026-04-21 16:15:24 +0200

    up pandoc-markdown-preserve-hard-line-breaks.sh

--- pandoc/pandoc-markdown-preserve-hard-line-breaks.sh
+++ pandoc/pandoc-markdown-preserve-hard-line-breaks.sh
@@ -6,8 +6,17 @@ if [ $# = 0 ]; then
   echo "error: no arguments"
   echo "example use:"
   echo "  $0 src.md -o dst.md"
+  echo "  $0 -i src.md"
   echo "  $0 src.md | sponge src.md"
   exit 1
+fi
+
+if [ $# = 2 ] && [ "$1" = "-i" ]; then
+  echo "editing in-place: $2" >&2
+  # sponge fails if $TMP directory does not exist
+  mkdir -p $TMP
+  pandoc --wrap=preserve -f gfm+hard_line_breaks -t gfm "$2" | sponge "$2"
+  exit $?
 fi
 
 exec pandoc --wrap=preserve -f gfm+hard_line_breaks -t gfm "$@"
